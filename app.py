fb041fcfc8b10fccc13ee16b6ce697c093b82525
tejashwini80 <kpatiltejashwini@gmail.com>
2024-09-04 15:45:10 +0530

    web app is ready

--- app.py
+++ app.py
@@ -30,10 +30,21 @@ def predict_api():
   data=request.json['data']    #data is a key
   print(data)
   print(np.array(list(data.values())).reshape(1,-1))
+  #standardization
   new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
   output=regmodel.predict(new_data)
   print(output[0])
   return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+   data=[float (x) for x in request.form.values()]
+   final_input=scaler.transform(np.array(data).reshape(1,-1))
+   print(final_input)
+   output=regmodel.predict(final_input)[0]
+   return render_template("home.html",prediction_text="The Predicted house price is {}".format(output))
+
+
 
 if __name__=="__main__":
   app.run(debug=True)
